7730f53f0959ab34713d711e4c1997295c7746bb
mohandand <mohandand@gmail.com>
2021-08-26 18:20:27 -0700

    updated smallest array program

---
+++ lowestinarray.js
@@ -0,0 +1,15 @@
+findSmallestInt(args) 
+{
+    var len = args.length;
+    var smallest = args[0];
+    for(var i = 0 ;i < len ; i++)
+      {
+        if(args[i]<=smallest)
+          {
+          smallest = args[i];
+          }
+      }
+      return smallest;
+}
+  console.log(findSmallestInt([4,2,6,1]));
+
